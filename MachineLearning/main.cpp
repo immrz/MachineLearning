568ec2fee40edc9ad27b1a5079fd21044b201c7c
毛润泽 <Mr.z@cernet.edu.cn>
2016-10-11 15:40:09 +0800

    Initial Commit

---
+++ MachineLearning/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  MachineLearning
+//
+//  Created by 毛润泽 on 16/10/11.
+//  Copyright © 2016年 毛润泽. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
